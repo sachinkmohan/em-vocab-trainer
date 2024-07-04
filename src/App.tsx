69a40260fe4a89d0c0529d79f18a61fc0649f359
Sachin KM <hiscan99@gmail.com>
2024-07-04 20:27:07 +0200

    create simple login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import SignUp from "./components/SignUp";
+// import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
 
 const App = () => {
   return (
-    <>
-      <SignUp />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 };
 
