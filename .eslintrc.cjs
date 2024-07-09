8ca1ff91ff5c0709fd93dfb463d64e105fd86033
Sachin KM <hiscan99@gmail.com>
2024-07-09 20:20:56 +0200

    additional jest config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node: true },
+  env: { browser: true, es2020: true, node: true, "jest/globals": true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
@@ -15,4 +15,9 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
+  overrides: [
+    {
+      env: { jest: true },
+    },
+  ],
 };
