ddea375f61100eb49d13576bc0fa6ae265549bd9
Sachin KM <hiscan99@gmail.com>
2024-07-20 15:26:45 +0200

    nav bar initial setup

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+const NavBar = () => {
+  const linkClass = ({ isActive }: { isActive: boolean }) =>
+    isActive
+      ? "bg-black text-white hover:bg-gray-900"
+      : "text-white hover:bg-gray-900 ";
+
+  return (
+    <div className="mx-auto max-w-7xl px-2 ">
+      <div className="flex h-20 items-center justify-between">
+        <div className="flex flex-1 items-center space-x-4">
+          <NavLink to="/home" className={linkClass}>
+            Home
+          </NavLink>
+          <NavLink to="/profile">Profile</NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
