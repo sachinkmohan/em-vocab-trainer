adb8c3344641a1b3b3ad9d7ce21a4db1c8bcafc9
Sachin KM <hiscan99@gmail.com>
2024-07-20 14:29:01 +0200

    create layouts and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 // import SignUp from "./components/SignUp";
 import Login from "./components/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainHomePage from "./pages/MainHomePage";
+import MainLayout from "./layouts/MainLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<MainLayout />}>
+        <Route index element={<MainHomePage />} />)
+      </Route>
+    </>
+  )
+);
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/pages/MainHomePage.tsx
@@ -0,0 +1,10 @@
+import HomePage from "../components/HomePage";
+const MainHomePage = () => {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+};
+
+export default MainHomePage;
