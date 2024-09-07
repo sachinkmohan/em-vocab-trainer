691332d65d4424aeb2ad16252a695e49965d1860
Sachin KM <hiscan99@gmail.com>
2024-09-07 17:46:09 +0200

    create fields

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -8,15 +8,13 @@ const SignUp = () => {
   const [nickname, setNickname] = useState("");
   const [name, setName] = useState("");
   const [learningLanguage, setLearningLanguage] = useState("");
-  const [level, setLevel] = useState("");
+  const [languageLevel, setLanguageLevel] = useState("");
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const handleSignUp = (e: React.FormEvent) => {
     e.preventDefault();
     const validationErrors = validateForm();
     if (Object.keys(validationErrors).length === 0) {
-      // signUpWithEmail(email, password);
-      // javascript alert - Sign up successful
       console.log("Sign up successful");
       alert("Sign up successful");
     } else {
@@ -27,6 +25,13 @@ const SignUp = () => {
   const validateForm = () => {
     const errors: { [key: string]: string } = {};
     if (!name) errors.name = "Name is required";
+    if (!nickname) errors.nickname = "Nickname is required";
+    if (!email) errors.email = "Email is required";
+    if (!password) errors.password = "Password is required";
+    if (password !== repeatPassword)
+      errors.repeatPassword = "Passwords do not match";
+    if (!learningLanguage) errors.learningLanguage = "One language is required";
+    if (!languageLevel) errors.languageLevel = "One level is required";
     return errors;
   };
 
@@ -35,10 +40,10 @@ const SignUp = () => {
       <div className="bg-white  p-6 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
         <form onSubmit={handleSignUp}>
-          <div className="py-2">
+          <div className="py-1">
             <input
               type="text"
-              placeholder="Name"
+              placeholder="Enter your name"
               value={name}
               onChange={(e) => setName(e.target.value)}
               className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 "
@@ -47,7 +52,21 @@ const SignUp = () => {
               <p className="text-red-500 text-sm">{errors.name}</p>
             )}
           </div>
-          <div>
+
+          <div className="py-1">
+            <input
+              type="text"
+              placeholder="Nick Name"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 "
+            ></input>
+            {errors.nickname && (
+              <p className="text-red-500 text-sm">{errors.nickname}</p>
+            )}
+          </div>
+
+          <div className="py-1">
             <input
               type="email"
               placeholder="Email"
@@ -56,9 +75,43 @@ const SignUp = () => {
               required
               className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
           </div>
 
-          <div className="py-2">
+          <div className="py-1">
+            <select
+              value={learningLanguage}
+              onChange={(e) => setLearningLanguage(e.target.value)}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
+            >
+              <option value="">Select Language</option>
+              <option value="malayalam">Malayalam</option>
+              <option value="kannada">Kannada</option>
+            </select>
+            {errors.learningLanguage && (
+              <p className="text-red-500 text-sm">{errors.learningLanguage}</p>
+            )}
+          </div>
+
+          <div className="py-1">
+            <select
+              value={languageLevel}
+              onChange={(e) => setLanguageLevel(e.target.value)}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
+            >
+              <option value="">Select Level</option>
+              <option value="beginner">Beginner</option>
+              <option value="intermediate">Intermediate</option>
+              <option value="advanced">Advanced</option>
+            </select>
+            {errors.languageLevel && (
+              <p className="text-red-500 text-sm">{errors.languageLevel}</p>
+            )}
+          </div>
+
+          <div className="py-1">
             <input
               type="password"
               placeholder="Password"
@@ -67,9 +120,27 @@ const SignUp = () => {
               required
               className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
             />
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password}</p>
+            )}
+          </div>
+
+          <div className="py-2">
+            <input
+              type="password"
+              placeholder="Repeat Password"
+              value={repeatPassword}
+              onChange={(e) => setRepeatPassword(e.target.value)}
+              required
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
+            />
+            {errors.repeatPassword && (
+              <p className="text-red-500 text-sm">{errors.repeatPassword}</p>
+            )}
           </div>
           <button
             type="submit"
+            onClick={handleSignUp}
             className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition-colors"
           >
             Sign Up
