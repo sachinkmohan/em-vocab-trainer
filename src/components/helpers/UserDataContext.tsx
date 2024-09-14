1e1688bceeca3318333504e237c8b050a91211e1
Sachin KM <hiscan99@gmail.com>
2024-09-14 12:44:19 +0200

    create user data context

--- src/components/helpers/UserDataContext.tsx
+++ src/components/helpers/UserDataContext.tsx
@@ -1,20 +1,84 @@
-// import { createContext, useState, useContext, ReactNode } from "react";
+import {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../utils/firebaseConfig";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+interface UserData {
+  email: string | null;
+  nickname: string | null;
+  name: string | null;
+  learningLanguage: string | null;
+  languageLevel: string | null;
+  roles: string[] | null;
+}
 
-// interface UserDataContextProps {
-//   email: string | null;
-//   setEmail: (email: string | null) => void;
-// }
+interface userDataContextProps extends UserData {
+  setUserData: (userData: UserData) => void; // KNOW
+}
 
-// const UserDataContext = createContext<UserDataContextProps | undefined>(
-//   undefined
-// );
+const UserDataContext = createContext<userDataContextProps | undefined>(
+  undefined
+);
 
-// export const UserDataPovider = ({ children }: { children: ReactNode }) => {
-//   const [email, setEmail] = useState<string | null>(null);
+export const UserDataProvider = ({ children }: { children: ReactNode }) => {
+  const [userData, setUserData] = useState<UserData>({
+    email: null,
+    nickname: null,
+    name: null,
+    learningLanguage: null,
+    languageLevel: null,
+    roles: null,
+  });
 
-//   return (
-//     <UserDataContext.Provider value={{ email, setEmail }}>
-//       {children}
-//     </UserDataContext.Provider>
-//   );
-// };
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      console.log("USER IN CONTEXT", user);
+      try {
+        if (user) {
+          const userDocRef = doc(db, "users", user.uid);
+
+          const userDoc = await getDoc(userDocRef);
+          if (userDoc.exists()) {
+            console.log("USERDOC", userDoc.data());
+            const userData = userDoc.data();
+            setUserData({
+              email: userData.email,
+              nickname: userData.nickname,
+              name: userData.name,
+              learningLanguage: userData.learningLanguage,
+              languageLevel: userData.languageLevel,
+              roles: userData.roles,
+            });
+          } else {
+            console.error("No such document");
+          }
+        }
+      } catch (error) {
+        console.error("Error getting document:", error);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <UserDataContext.Provider value={{ ...userData, setUserData }}>
+      {children}
+    </UserDataContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useUserData = () => {
+  const context = useContext(UserDataContext);
+  if (!context) {
+    throw new Error("useUserData must be used within a UserDataProvider");
+  }
+  return context;
+};
