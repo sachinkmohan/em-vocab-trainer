a2ed2c0407809ad5df700e4c167cfcd8dea61fa4
Sachin KM <hiscan99@gmail.com>
2024-09-28 12:56:26 +0200

    EMV-77 - fetch and display user growth points (GP)

--- src/components/dashboard/UserAllTimeDashboard.tsx
+++ src/components/dashboard/UserAllTimeDashboard.tsx
@@ -1,65 +1,64 @@
 import { db } from "../../utils/firebaseConfig";
 import { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useUserData } from "../helpers/UserDataContext";
 
 interface userData {
   userID: string;
-  userPrimaryLanguage: string;
+  userGrowthPoints: number;
   name: string;
-  favoriteWordsCount: number;
 }
 
 const AdminDashboard = () => {
   const [usersData, setUsersData] = useState<userData[]>([]);
+  const { learningLanguage } = useUserData();
 
   useEffect(() => {
     const fetchUsersData = async () => {
       try {
         const usersCollectionRef = collection(db, "users");
-        const usersSnapshot = await getDocs(usersCollectionRef);
+        // Create a query to filter users by the current user's learningLanguage
+        const usersQuery = query(
+          usersCollectionRef,
+          where("learningLanguage", "==", learningLanguage)
+        );
+        const usersSnapshot = await getDocs(usersQuery);
         const userData = await Promise.all(
           usersSnapshot.docs.map(async (userDoc) => {
             const userData = userDoc.data();
-            const favoriteWordIDsCollectionRef = collection(
-              userDoc.ref,
-              "favoriteWordIDs"
-            );
-            const favoriteWordsSnapshot = await getDocs(
-              favoriteWordIDsCollectionRef
-            );
-            const favoriteWordsCount = favoriteWordsSnapshot.size;
+
             return {
               userID: userDoc.id,
-              userPrimaryLanguage: userData.learningLanguage,
+              userGrowthPoints: userData.growthPoints || 0,
               name: userData.name,
-              favoriteWordsCount,
             };
           })
         );
-        setUsersData(userData);
+        const sortedUserData = userData.sort(
+          (a, b) => b.userGrowthPoints - a.userGrowthPoints
+        );
+        setUsersData(sortedUserData);
       } catch (error) {
         console.error("Error fetching users data: ", error);
       }
     };
     fetchUsersData();
-  }, []);
+  }, [learningLanguage]);
   return (
     <div className="p-6 bg-gray-100 rounded-lg shadow-md m-4">
-      <h1>Admin Dashboard</h1>
+      <h1>GP Dashboard</h1>
       <table>
         <thead>
           <tr>
             <th className="py-2 px-4 border-b"> Name</th>
-            <th className="py-2 px-4 border-b"> Primary Language</th>
-            <th className="py-2 px-4 border-b"> Fav Count</th>
+            <th className="py-2 px-4 border-b"> Growth Points(GP)</th>
           </tr>
         </thead>
         <tbody>
           {usersData.map((user) => (
             <tr key={user.userID}>
               <td className="py-2 px-4 border-b">{user.name}</td>
-              <td className="py-2 px-4 border-b">{user.userPrimaryLanguage}</td>
-              <td className="py-2 px-4 border-b">{user.favoriteWordsCount}</td>
+              <td className="py-2 px-4 border-b">{user.userGrowthPoints}</td>
             </tr>
           ))}
         </tbody>
