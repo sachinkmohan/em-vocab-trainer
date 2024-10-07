695ccb2199f3368e6094f3781688bd8c3c16aa66
Sachin KM <hiscan99@gmail.com>
2024-10-07 22:09:33 +0200

    EMV-82 - remove sort after favoriting

--- src/components/words/WordListContainer.tsx
+++ src/components/words/WordListContainer.tsx
@@ -34,26 +34,18 @@ const WordListContainer: React.FC<WordListContainerProps> = ({
 }) => {
   return (
     <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {words
-        //@ts-expect-error - TS doesn't know about toSorted even after adding to tsconfig.json
-        .toSorted((a: Word, b: Word) => {
-          const aFavorited = favoriteWords.includes(a.id);
-          const bFavorited = favoriteWords.includes(b.id);
-          if (aFavorited && !bFavorited) return 1;
-          if (!aFavorited && bFavorited) return -1;
-          return 0;
-        })
-        .map((word: Word) => {
-          return (
-            <WordItem
-              word={word}
-              highlightedWordId={highlightedWordId}
-              favoriteWords={favoriteWords}
-              handleFavoriteClick={handleFavoriteClick}
-              handleInfoClick={handleInfoClick}
-            />
-          );
-        })}
+      {words.map((word: Word) => {
+        return (
+          <WordItem
+            key={word.id}
+            word={word}
+            highlightedWordId={highlightedWordId}
+            favoriteWords={favoriteWords}
+            handleFavoriteClick={handleFavoriteClick}
+            handleInfoClick={handleInfoClick}
+          />
+        );
+      })}
     </ul>
   );
 };
