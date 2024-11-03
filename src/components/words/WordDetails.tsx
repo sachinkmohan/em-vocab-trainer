b5cc19cd9a3d84a9185e13e42f6890483cc03a42
Sachin KM <hiscan99@gmail.com>
2024-11-03 23:42:15 +0100

    EMV-83 - better styling

--- src/components/words/WordDetails.tsx
+++ src/components/words/WordDetails.tsx
@@ -30,30 +30,38 @@ const WordDetails = forwardRef<HTMLDialogElement, WordDetailsProps>(
         ref={ref}
         className="fixed max-w-md h-1/3 bg-gray-50 p-4 rounded-lg shadow-xl border border-gray-200 w-11/12  "
       >
-        <div className="flex flex-col items-start">
+        <div className="flex flex-col items-start pl-3">
           <div className="flex flex-row text-2xl pt-5">
-            <h2 className="pr-2 text-bol">Word:</h2>
+            <h2 className="pr-2 font-bold text-blue-600">Word:</h2>
             <h2>{selectedWord.word.inTranslit}</h2>
             <h2>({selectedWord.word.inNativeScript})</h2>
           </div>
-          <h5 className="pl-16 text-sm">{selectedWord.figureOfSpeech}</h5>
+          <h5 className="pl-16 text-sm text-gray-600">
+            {selectedWord.figureOfSpeech}
+          </h5>
 
           <div className="flex text-xl">
-            <h2 className="pr-2">Translation:</h2>
-            <h2>{selectedWord.meaning}</h2>
+            <h2 className="pr-2 font-semibold text-green-600">Translation:</h2>
+            <h2 className="text-gray-600">{selectedWord.meaning}</h2>
           </div>
 
           <div className="flex text-lg mt-4">
-            <h2 className="pr-2">Eg(T).:</h2>
-            <h2>{selectedWord.examples[0].inTranslit}</h2>
+            <h2 className="pr-2 font-semibold text-purple-600">Eg(T).:</h2>
+            <h2 className="text-gray-600">
+              {selectedWord.examples[0].inTranslit}
+            </h2>
           </div>
           <div className="flex text-lg">
-            <h2 className="pr-2">Eg(N).:</h2>
-            <h2>{selectedWord.examples[0].inNativeScript}</h2>
+            <h2 className="pr-2 font-semibold text-purple-600">Eg(N).:</h2>
+            <h2 className="text-gray-600">
+              {selectedWord.examples[0].inNativeScript}
+            </h2>
           </div>
           <div className="flex text-lg">
-            <h2 className="pr-2">Transl.:</h2>
-            <h2>{selectedWord.examples[0].translation}</h2>
+            <h2 className="pr-2 font-semibold text-purple-600">Transl.:</h2>
+            <h2 className="text-gray-600">
+              {selectedWord.examples[0].translation}
+            </h2>
           </div>
         </div>
 
