f01d7c1d07814d6429bac751adefad8271c4cd53
Sachin KM <hiscan99@gmail.com>
2024-11-15 20:13:57 +0100

    EMV-95 - Implement a simple bottom navigation bar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import BottomNavBar from "../components/BottomNavBar";
 
 const MainLayout = () => {
   return (
     <>
       <NavBar />
+      <BottomNavBar />
       <Outlet />
     </>
   );
