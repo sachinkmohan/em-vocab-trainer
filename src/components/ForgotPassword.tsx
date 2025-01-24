9cf2bfd6085eeee5db2c1a84c69f16789f510b50
Sachin KM <hiscan99@gmail.com>
2025-01-24 23:55:41 +0100

    EMV-20 - add Forgot Password functionality with email input and feedback message

--- src/components/ForgotPassword.tsx
+++ src/components/ForgotPassword.tsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [sentEmailAddress, setSentEmailAddress] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const auth = getAuth();
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setMessage("Password reset email sent");
+      setSentEmailAddress(email);
+      setEmail("");
+    } catch (error) {
+      setMessage("Error sending password reset email.");
+    }
+  };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 ">Reset your account</h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <input
               type="email"
@@ -19,9 +35,23 @@ const ForgotPassword = () => {
             />
           </div>
           <button className="w-full bg-blue-500 text-white py-2 rounded-md">
-            Reset password
+            Send reset password link
           </button>
         </form>
+
+        {message && (
+          <div
+            className={`text-center mt-4  p-4 rounded ${
+              message.includes("sent")
+                ? "text-green-700 bg-green-100"
+                : "text-red-700 bg-red-100"
+            }`}
+          >
+            {message.includes("sent")
+              ? `Password reset email sent to ${sentEmailAddress}`
+              : message}
+          </div>
+        )}
       </div>
     </div>
   );
