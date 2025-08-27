1a54c06e9de36feba94a862bfe4004626ff7e6fd
Sachin KM <hiscan99@gmail.com>
2025-08-27 13:03:29 +0200

    Refactor UserFeed component to include date formatting and sorting, and enhance rendering with user icons

--- src/pages/UserFeed.tsx
+++ src/pages/UserFeed.tsx
@@ -1,9 +1,9 @@
-// import { FaCheckCircle } from "react-icons/fa"; // Importing necessary icons
-// import { formatDistanceToNow, compareDesc, parse } from "date-fns";
+import { formatDistanceToNow, compareDesc, parse } from "date-fns";
 import { db } from "../utils/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { IoPersonCircleOutline } from "react-icons/io5";
 
 interface UserFeed {
   id: string;
@@ -11,78 +11,12 @@ interface UserFeed {
   message: string;
 }
 
-// interface TimelineItem {
-//   date: string;
-//   user: string;
-// }
 const UserFeed = () => {
   const [userFeedData, setUserFeedData] = useState<UserFeed[]>([]);
 
-  //   const parseDate = (date: string) => {
-  //     return parse(date, "dd-MM-yyyy", new Date());
-  //   };
-
-  //   const timelineData: TimelineItem[] = [
-  //     {
-  //       date: "06-02-2025",
-  //       user: "John Doe",
-  //     },
-  //     {
-  //       date: "01-01-2025",
-  //       user: "Susan Smith",
-  //     },
-  //     // generate 10 more random dates
-  //     {
-  //       date: "15-03-2025",
-  //       user: "Alice Johnson",
-  //     },
-  //     {
-  //       date: "20-04-2025",
-  //       user: "Bob Brown",
-  //     },
-  //     {
-  //       date: "25-05-2025",
-  //       user: "Charlie Davis",
-  //     },
-  //     {
-  //       date: "30-06-2025",
-  //       user: "Diana Evans",
-  //     },
-  //     {
-  //       date: "05-07-2025",
-  //       user: "Ethan Foster",
-  //     },
-  //     {
-  //       date: "10-08-2025",
-  //       user: "Fiona Green",
-  //     },
-  //     {
-  //       date: "15-09-2025",
-  //       user: "George Harris",
-  //     },
-  //     {
-  //       date: "20-10-2025",
-  //       user: "Hannah Ives",
-  //     },
-  //     {
-  //       date: "25-11-2025",
-  //       user: "Ian Johnson",
-  //     },
-  //     {
-  //       date: "30-12-2025",
-  //       user: "Jack King",
-  //     },
-  //   ].sort((a, b) => {
-  //     // Sorting by date in DD-MM-YYYY format
-  //     const dateA = parseDate(a.date);
-  //     const dateB = parseDate(b.date);
-  //     return compareDesc(dateA, dateB);
-  //   });
-
-  //   const formatRelativeTime = (date: string) => {
-  //     const parsedDate = parseDate(date);
-  //     return formatDistanceToNow(parsedDate, { addSuffix: true });
-  //   };
+  const parseDate = (date: string) => {
+    return parse(date, "dd-MM-yyyy", new Date());
+  };
 
   useEffect(() => {
     const fetchUserFeed = async () => {
@@ -105,13 +39,36 @@ const UserFeed = () => {
   }, []);
 
   const renderUserFeedData = () => {
-    return userFeedData.map((ufData) => (
-      <p key={ufData.id}>{ufData.message}</p>
+    const sortedUserFeedDataWithDate = [...userFeedData].sort(
+      (a: UserFeed, b: UserFeed) => {
+        if (a.date !== "" && b.date !== "") {
+          return compareDesc(parseDate(a.date), parseDate(b.date));
+        }
+        if (a.date === "" && b.date === "") return 0;
+        return a.date === "" ? 1 : -1;
+      }
+    );
+    return sortedUserFeedDataWithDate.map((ufData) => (
+      <div key={ufData.id} className="flex items-center gap-2">
+        <IoPersonCircleOutline className="text-6xl" />
+        <div>
+          <p>{ufData.message}</p>
+          {/* <p>{ufData.date}</p> */}
+          {ufData.date && (
+            <p className="text-sm text-gray-500">
+              {formatDistanceToNow(
+                parse(ufData.date, "dd-MM-yyyy", new Date()),
+                { addSuffix: true }
+              )}
+            </p>
+          )}
+        </div>
+      </div>
     ));
   };
 
   return (
-    <div className="max-w-2xl mx-auto px-8">
+    <div className="max-w-2xl mx-auto px-8 pb-16">
       <div className="py-2">
         <h2 className="text-xl font-bold py-2">Scrolls</h2>
       </div>
